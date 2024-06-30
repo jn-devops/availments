d4fb29a6b0d029a9e4b9fdf7ee7e2f6c140c78d5
jn-devops <devops@joy-nostalg.com>
2024-06-30 15:32:44 +0800

    configure settings in avail loan processing service action

--- config/availments.php
+++ config/availments.php
@@ -1,7 +1,8 @@
 <?php
 
-// config for Homeful/Availments
 return [
-    'default_percent_down_payment' => env('DEFAULT_PERCENT_DOWN_PAYMENT', 10 / 100),
-    'default_total_contract_price_balance_down_payment_term' => env('DEFAULT_TCP_BALANCE_DOWN_PAYMENT_TERM', 6),
+    'default_percent_down_payment' => env('DEFAULT_PERCENT_DOWN_PAYMENT', 5 / 100),
+    'default_percent_miscellaneous_fees' => env('DEFAULT_PERCENT_MISCELLANEOUS_FEES', 8.5 / 100),
+    'default_loan_term' => env('DEFAULT_LOAN_TERM', 20),
+    'default_total_contract_price_balance_down_payment_term' => env('DEFAULT_TCP_BALANCE_DOWN_PAYMENT_TERM', 12),
 ];
--- src/Actions/AvailLoanProcessingServiceAction.php
+++ src/Actions/AvailLoanProcessingServiceAction.php
@@ -21,6 +21,90 @@ class AvailLoanProcessingServiceAction
 {
     use AsAction;
 
+    protected float $percent_down_payment;
+
+    protected float $percent_miscellaneous_fees;
+
+    protected int $loan_term;
+
+    protected int $total_contract_price_balance_down_payment_term;
+
+    /**
+     * @param float $percent_down_payment
+     * @return $this
+     */
+    public function setPercentDownPayment(float $percent_down_payment): self
+    {
+        $this->percent_down_payment = $percent_down_payment;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPercentDownPayment(): float
+    {
+        return $this->percent_down_payment ?? config('availments.default_percent_down_payment');
+    }
+
+    /**
+     * @param float $percent_miscellaneous_fees
+     * @return $this
+     */
+    public function setPercentMiscellaneousFees(float $percent_miscellaneous_fees): self
+    {
+        $this->percent_miscellaneous_fees = $percent_miscellaneous_fees;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPercentMiscellaneousFees(): float
+    {
+        return $this->percent_miscellaneous_fees ?? config('availments.default_percent_miscellaneous_fees');
+    }
+
+    /**
+     * @param int $loan_term
+     * @return $this
+     */
+    public function setLoanTerm(int $loan_term): self
+    {
+        $this->loan_term = $loan_term;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLoanTerm(): int
+    {
+        return $this->loan_term ?? config('availments.default_loan_term');
+    }
+
+    /**
+     * @param int $total_contract_price_balance_down_payment_term
+     * @return $this
+     */
+    public function setTotalContractPriceBalanceDownPaymentTerm(int $total_contract_price_balance_down_payment_term): AvailLoanProcessingServiceAction
+    {
+        $this->total_contract_price_balance_down_payment_term = $total_contract_price_balance_down_payment_term;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalContractPriceBalanceDownPaymentTerm(): int
+    {
+        return $this->total_contract_price_balance_down_payment_term ?? config('availments.default_total_contract_price_balance_down_payment_term');
+    }
+
     /**
      * @throws MaximumBorrowingAgeBreached
      * @throws MinimumBorrowingAgeNotMet
@@ -62,11 +146,11 @@ class AvailLoanProcessingServiceAction
         $product_sku = $propertyObject->getSKU();
         $holding_fee = Arr::get($validated, 'holding_fee', $propertyObject->getProcessingFee());
         $total_contract_price = Arr::get($validated, 'total_contract_price', $property->getTotalContractPrice());
-        $percent_down_payment = Arr::get($validated, 'percent_down_payment', 5 / 100);
-        $percent_miscellaneous_fees = Arr::get($validated, 'percent_miscellaneous_fees', 8.5 / 100);
+        $percent_down_payment = Arr::get($validated, 'percent_down_payment', $this->getPercentDownPayment());
+        $percent_miscellaneous_fees = Arr::get($validated, 'percent_miscellaneous_fees', $this->getPercentMiscellaneousFees());
         $loan_interest = Arr::get($validated, 'loan_interest', $loan->getAnnualInterestRate());
-        $loan_term = Arr::get($validated, 'loan_term', 20);
-        $total_contract_price_balance_down_payment_term = Arr::get($validated, 'total_contract_price_balance_down_payment_term', 12);
+        $loan_term = Arr::get($validated, 'loan_term', $this->getLoanTerm());
+        $total_contract_price_balance_down_payment_term = Arr::get($validated, 'total_contract_price_balance_down_payment_term', $this->getTotalContractPriceBalanceDownPaymentTerm());
         $low_cash_out_amount = Arr::get($validated, 'low_cash_out_amount', 0);
 
         $attribs = [
@@ -86,4 +170,6 @@ class AvailLoanProcessingServiceAction
             $availment->save();
         });
     }
+
+
 }
